377de0734b2c7c04366ec7acbf631a39a97aab3e
Dave Beck <dacb@uw.edu>
2016-05-24 15:01:27 -0700

    generalized several lines from project specific pieces

--- globals.sh
+++ globals.sh
@@ -1,31 +1,32 @@
 # to be sourced
 
-D5P=~dacb/drive5
+D5P=/work/software/drive5
 USEARCH=$D5P/usearch7.0.1001_i86linux32
 FQ2FASTQ=$D5P/py/faqual2fastq.py
 FQRELABEL=$D5P/py/fastq_strip_barcode_relabel2.py
 NUMBER=$D5P/py/fasta_number.py
 UC2OTUTAB=$D5P/py/uc2otutab.py
 
-RDPT=~dacb/RDPTools
+RDPT=/work/software/RDPTools
 CLFRJAR=$RDPT/classifier.jar
 
 # e.g.
 # the mysql_host command should echo the hostname of the mysql server
 HOST=`mysql_host`
-DB=ribosomal_profile_db
+DB=saliva
 
 # e.g.
-RUNS="082214MC27Fillumina 070114MC27Fillumina"
+RUNS="040816JP27F"
 PRIMER=AGRGTTTGATCMTGGCTCAG
 
 # adjust for your data
-TRUNCLEN=492
-MAXEE=0.5
+TRUNCLEN=470
+MAXEE=2
 CLUSTER_IDENT=0.95
 
 SAMPLE_INFO=sample_info.xls
 SAMPLE_INFO_SUBSET=sample_info.subset.xls
 
 # e.g.
-OUTPUT_NAME=ribosomal_profile
+OUTPUT_NAME=saliva
+PERSISTENT_BEST_PCNT=.50
